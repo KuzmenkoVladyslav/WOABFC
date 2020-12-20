f0986c358ec0725cf8d7cdda2342bbd9eabe59d6
vlad_kuzmenko <ha4ajlbhuke@gmail.com>
2020-12-20 09:59:28 +0200

    #000055: increased cost of new eras upgrade by 1

--- WOABFC/Player.cpp
+++ WOABFC/Player.cpp
@@ -113,31 +113,31 @@ void Player::setPlayerEra(enumEraName playerEra)
 
     case enumEraName::ERA_MEDIVAL:
         this->playerReserveCount = 4;
-        this->playerNextEraCost = 5;
+        this->playerNextEraCost = 6;
         this->playerActionPointsMax = 4;
         break;
 
     case enumEraName::ERA_RENAISSANCEE:
         this->playerReserveCount = 5;
-        this->playerNextEraCost = 5;
+        this->playerNextEraCost = 6;
         this->playerActionPointsMax = 4;
         break;
 
     case enumEraName::ERA_INDUSTRIAL:
         this->playerReserveCount = 5;
-        this->playerNextEraCost = 6;
+        this->playerNextEraCost = 7;
         this->playerActionPointsMax = 5;
         break;
 
     case enumEraName::ERA_MODERN:
         this->playerReserveCount = 6;
-        this->playerNextEraCost = 6;
+        this->playerNextEraCost = 7;
         this->playerActionPointsMax = 5;
         break;
 
     case enumEraName::ERA_ATOMIC:
         this->playerReserveCount = 6;
-        this->playerNextEraCost = 7;
+        this->playerNextEraCost = 8;
         this->playerActionPointsMax = 6;
         break;
 
